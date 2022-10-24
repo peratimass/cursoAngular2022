10b189e348317adffca1845648756d440ded4979
perantimass <percy19_is@hotmail.com>
2022-10-24 01:05:15 -0500

    Clase 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface Tarjeta{
   titulo:string;
   subtitulo:string;
-  nro?:number;
+  image:string;
 }
 
 @Component({
@@ -17,11 +17,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit():void{
       this.ArregloTarjetas=[
-          {titulo:'Video1',subtitulo:'Subtitulo Video 1',nro:1},
-          {titulo:'Video2',subtitulo:'Subtitulo Video 2'},
-          {titulo:'Video3',subtitulo:'Subtitulo Video 3'},
-          {titulo:'Video4',subtitulo:'Subtitulo Video 4'},
-          {titulo:'Video5',subtitulo:'Subtitulo Video 5'}
+          {titulo:'Video1',subtitulo:'Subtitulo Video 1',image:"https://keyfacil.com/wp-content/uploads/2022/04/banner-bg.jpg"},
+          {titulo:'Video2',subtitulo:'Subtitulo Video 2',image:"https://keyfacil.com/wp-content/uploads/2022/03/keyman-02-02.png"},
+          {titulo:'Video3',subtitulo:'Subtitulo Video 3',image:"https://e.rpp-noticias.io/large/2022/04/27/394139_1251575.jpg"},
+          {titulo:'Video4',subtitulo:'Subtitulo Video 4',image:"https://www.credibanco.com/wp-content/uploads/2021/03/ImagenContactoCredibanco.jpg"},
+          {titulo:'Video5',subtitulo:'Subtitulo Video 5',image:"https://migrate.info/py/wp-content/uploads/2019/04/original-afcb71ac8f1d4d633ee1a723489f95b4-1.jpg"}
       ]
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,12 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  public image:string="https://keyfacil.com/wp-content/uploads/2022/03/keyfacil-devices-32.png";
+  public image?:string;
   public Titulo:string="Curso de Angular con Interpolacion";
+  @Input() dataEntrante:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.image="https://keyfacil.com/wp-content/uploads/2022/03/keyfacil-devices-32.png";
   }
 
 }
